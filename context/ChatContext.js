81dab96b2fe2c4589d6df8e928aab88ef2b5f629
emmyteo <emmy.lingxin@gmail.com>
2024-04-10 15:44:08 +0800

    Add files via upload

--- context/ChatContext.js
+++ context/ChatContext.js
@@ -29,7 +29,7 @@ export const ChatProvider = ({ children }) => {
       }
       let chainId = await ethereum.request({ method: "eth_chainId" });
       console.log("Connected to " + chainId);
-      const rinkebyChainId = "0x1";
+      const rinkebyChainId = "0xaa36a7";
       if (chainId !== rinkebyChainId) {
         console.log("Please connect to Goerli Test Network");
         setCorrectNetwork(false);
